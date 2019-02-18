b66da882b78a8efffa53a4813903dabbe65a753d
Devanshi Parikh <devanshi@Devanshis-MacBook-Pro.local>
2019-02-17 18:42:30 -0600

    adding more details and styles

---
+++ static/js/slideshow.js
@@ -0,0 +1,56 @@
+var slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+    showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+    showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+    var i;
+    var slides = document.getElementsByClassName("mySlides");
+    var dots = document.getElementsByClassName("dot");
+    if (n > slides.length) {slideIndex = 1}
+    if (n < 1) {slideIndex = slides.length}
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
+    slides[slideIndex-1].style.display = "block";
+    dots[slideIndex-1].className += " active";
+}
+
+
+// Experience Slideshow
+
+var slideIndex1 = 1;
+showSlides1(slideIndex1);
+
+function plusSlides1(n) {
+    showSlides1(slideIndex1 += n);
+}
+
+function currentSlide1(n) {
+    showSlides1(slideIndex1 = n);
+}
+
+function showSlides1(n) {
+    var i1;
+    var slides1 = document.getElementsByClassName("mySlides1");
+    var dots1 = document.getElementsByClassName("dot1");
+    if (n > slides1.length) {slideIndex1 = 1}
+    if (n < 1) {slideIndex1 = slides1.length}
+    for (i1 = 0; i1 < slides1.length; i1++) {
+        slides1[i1].style.display = "none";
+    }
+    for (i1 = 0; i1 < dots1.length; i1++) {
+        dots1[i1].className = dots1[i1].className.replace(" active", "");
+    }
+    slides1[slideIndex1-1].style.display = "block";
+    dots1[slideIndex1-1].className += " active";
+}
