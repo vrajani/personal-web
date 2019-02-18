716b227571425b52fd8dfe709217092f342a262a
Devanshi Parikh <devanshi@Devanshis-MacBook-Pro.local>
2019-02-18 12:36:39 -0600

    adding the mechanism to save contact details

--- static/js/main.js
+++ static/js/main.js
@@ -6,29 +6,40 @@ const mongodb = client.getServiceClient(
 	"mongodb-atlas"
 );
 // Get a reference to the blog database
-const db = mongodb.db("blog");
+const db = mongodb.db("profile");
 
-function displayContents() {
-	db.collection("contents")
-		.find({"key":"skills"}, { limit: 1 })
-		.then(docs => {
-			const html = docs.map(doc => `<div>${doc.comment}</div>`);
-			document.getElementById("comments").innerHTML = html;
+function displayConfirmation() {
+	const confirmation = document.getElementById("confirmation");
+	const html = `<div>Your message was successfully received! Have a nice day!</div>`;
+	confirmation.innerHTML = html;
+
+}
+
+function addContact() {
+	const firstName = document.getElementById("first_name");
+	const lastName = document.getElementById("last_name");
+	const email = document.getElementById("contact_email");
+	const phone = document.getElementById("contact_phone");
+	const reason = document.getElementById("reason");
+	const companyName = document.getElementById("company_name");
+
+	console.log("add contact", client.auth.user.id)
+	displayConfirmation();
+	db.collection("contacts")
+		.insertOne({
+			owner_id: client.auth.user.id,
+			firstName: firstName.value,
+			lastName: lastName.value,
+			email: email.value,
+			phone: phone.value,
+			reason: phone.reason,
+			companyName: phone.companyName,
+			dateTime : Date.now()
 		})
-}
-
-function displayContentsOnLoad() {
-	client.auth
-		.loginWithCredential(new stitch.AnonymousCredential())
-		.then(displayContents)
-		.catch(console.error);
-}
-
-function addComment() {
-	const newComment = document.getElementById("new_comment");
-	console.log("add comment", client.auth.user.id)
-	db.collection("comments")
-		.insertOne({ owner_id: client.auth.user.id, comment: newComment.value })
-		.then(displayComments);
-	newComment.value = "";
-}
+		.then(displayConfirmation);
+	firstName.value = "";
+	lastName.value = "";
+	email.value = "";
+	phone.value = "";
+	reason.value = "";
+	companyName.value = "";}
